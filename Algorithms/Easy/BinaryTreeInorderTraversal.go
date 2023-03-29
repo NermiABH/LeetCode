13350c69ca8b23fbd3768dd675f2d5346732e2d7
NermiABH <swapsanssmyr@gmail.com>
2023-03-29 19:31:02 +0300

    Add BinaryTreeInorderTraversal

---
+++ Algorithms/Easy/BinaryTreeInorderTraversal.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	ans := make([]int, 0)
+	helper(root, &ans)
+	return ans
+}
+
+func helper(root *TreeNode, ans *[]int) {
+	if root == nil {
+		return
+	}
+	helper(root.Left, ans)
+	*ans = append(*ans, root.Val)
+	helper(root.Right, ans)
+}
