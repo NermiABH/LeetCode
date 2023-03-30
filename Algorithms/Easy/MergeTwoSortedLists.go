833ccebc9ad79e27f2269b7214e6d97053d3ac1e
NermiABH <swapsanssmyr@gmail.com>
2023-03-30 21:33:17 +0300

    Fix MergeTwoSortedLists

--- Algorithms/Easy/MergeTwoSortedLists.go
+++ Algorithms/Easy/MergeTwoSortedLists.go
@@ -27,10 +27,10 @@ func mergeTwoListsRec(list1 *ListNode, list2 *ListNode) *ListNode {
 	} else if list2 == nil {
 		return list1
 	} else if list1.Val <= list2.Val {
-		list1.Next = mergeTwoLists(list1.Next, list2)
+		list1.Next = mergeTwoListsRec(list1.Next, list2)
 		return list1
 	} else {
-		list2.Next = mergeTwoLists(list1, list2.Next)
+		list2.Next = mergeTwoListsRec(list1, list2.Next)
 		return list2
 	}
 }
