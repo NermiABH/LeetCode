1837c954ec366cf07f05a55baecc6c82c944c939
NermiABH <swapsanssmyr@gmail.com>
2023-03-31 11:36:21 +0300

    Add LinkedListCycle

---
+++ Algorithms/Easy/LinkedListCycle.go
@@ -0,0 +1,12 @@
+package main
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
