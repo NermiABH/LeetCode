6a8ad9031d288902d44b12a283158dddcd91b13b
NermiABH <swapsanssmyr@gmail.com>
2023-05-27 23:42:32 +0300

    Add Iterative solution in BinaryTreePostorderTraversal

--- Algorithms/Easy/BinaryTreePostorderTraversal.go
+++ Algorithms/Easy/BinaryTreePostorderTraversal.go
@@ -1,6 +1,6 @@
 package main
 
-func postorderTraversal(root *TreeNode) []int {
+func postorderTraversalRecursive(root *TreeNode) []int {
 	var slice []int
 	var helper func(root *TreeNode, slice *[]int)
 	helper = func(root *TreeNode, slice *[]int) {
@@ -13,4 +13,39 @@ func postorderTraversal(root *TreeNode) []int {
 	}
 	helper(root, &slice)
 	return slice
+}
+
+func postorderTraversalIterative(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	stack, result := make([]*TreeNode, 0), make([]int, 0)
+	stack = append(stack, root)
+	for {
+		if root != nil {
+			if root.Right != nil {
+				stack = append(stack, root.Right)
+			}
+			if root.Left != nil {
+				stack = append(stack, root.Left)
+				root = root.Left
+			} else {
+				root = root.Right
+			}
+		} else {
+			root, stack = stack[len(stack)-1], stack[0:len(stack)-1]
+		Loop:
+			result = append(result, root.Val)
+			if len(stack) == 0 {
+				return result
+			}
+			peek := stack[len(stack)-1]
+			if peek.Left == root || peek.Right == root {
+				stack = stack[0 : len(stack)-1]
+				root = peek
+				goto Loop
+			}
+			root = peek
+		}
+	}
 }
