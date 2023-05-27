dfeff501b0f44a2e5c8f779da99496f3924ad9aa
NermiABH <swapsanssmyr@gmail.com>
2023-05-27 19:14:50 +0300

    Add Iterative solution

--- Algorithms/Easy/BinaryTreeInorderTraversal.go
+++ Algorithms/Easy/BinaryTreeInorderTraversal.go
@@ -1,6 +1,6 @@
 package main
 
-func inorderTraversal(root *TreeNode) []int {
+func inorderTraversalRecursive(root *TreeNode) []int {
 	ans := make([]int, 0)
 	helper(root, &ans)
 	return ans
@@ -13,4 +13,19 @@ func helper(root *TreeNode, ans *[]int) {
 	helper(root.Left, ans)
 	*ans = append(*ans, root.Val)
 	helper(root.Right, ans)
+}
+
+func inorderTraversalIterative(root *TreeNode) []int {
+	stack := make([]*TreeNode, 0)
+	res := make([]int, 0)
+	for len(stack) != 0 || root != nil {
+		if root != nil {
+			stack, root = append(stack, root), root.Left
+		} else {
+			root, stack = stack[len(stack)-1], stack[0:len(stack)-1]
+			res = append(res, root.Val)
+			root = root.Right
+		}
+	}
+	return res
 }
