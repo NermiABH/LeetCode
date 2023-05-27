74220b520bc3e16d31a327c05d44f60fd5760349
NermiABH <swapsanssmyr@gmail.com>
2023-05-28 00:02:33 +0300

    Add Iterative solution in BinaryTreePreorderTraversal.go

--- Algorithms/Easy/BinaryTreePreorderTraversal.go
+++ Algorithms/Easy/BinaryTreePreorderTraversal.go
@@ -1,6 +1,6 @@
 package main
 
-func preorderTraversal(root *TreeNode) []int {
+func preorderTraversalRecursive(root *TreeNode) []int {
 	var slice []int
 	var helper func(root *TreeNode, slice *[]int)
 	helper = func(root *TreeNode, slice *[]int) {
@@ -13,4 +13,22 @@ func preorderTraversal(root *TreeNode) []int {
 	}
 	helper(root, &slice)
 	return slice
+}
+
+func preorderTraversalIterative(root *TreeNode) []int {
+	stack, result := make([]*TreeNode, 0), make([]int, 0)
+	for {
+		if root != nil {
+			result = append(result, root.Val)
+			if root.Right != nil {
+				stack = append(stack, root.Right)
+			}
+			root = root.Left
+		} else {
+			if len(stack) == 0 {
+				return result
+			}
+			root, stack = stack[len(stack)-1], stack[0:len(stack)-1]
+		}
+	}
 }
