908a80da9e61bf71faec2b43b912b7c28d020a9a
NermiABH <93516873+NermiABH@users.noreply.github.com>
2025-01-04 15:05:37 +0300

    [605] [Time Beats: 100.00%] [Memory Beats: 44.77%] - LeetPush

--- 605-Can-Place-Flowers.go
+++ 605-Can-Place-Flowers.go
@@ -1,11 +1,17 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
+    if n == 0{
+        return true
+    }
 	for i, v := range flowerbed {
         if (i == 0 || flowerbed[i-1] == 0) && 
         (len(flowerbed) - 1 == i || flowerbed[i+1] == 0) && 
         v == 0 {
             flowerbed[i] = 1
             n--
+            if n == 0 {
+                return true
+            }
         }
 	}
-	return n <= 0
+	return false
 }
