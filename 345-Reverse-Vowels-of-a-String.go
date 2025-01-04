60fb34210b3b2ecc71161e5ec9155139e0c92b08
NermiABH <93516873+NermiABH@users.noreply.github.com>
2025-01-04 15:43:26 +0300

    [345] [Time Beats: 100.00%] [Memory Beats: 87.06%] - LeetPush

---
+++ 345-Reverse-Vowels-of-a-String.go
@@ -0,0 +1,24 @@
+func reverseVowels(s string) string {
+	b := []byte(s)
+	for i, j := 0, len(b)-1; i < j; {
+		if !isVowel(s[i]) {
+			i++
+			continue
+		}
+		if !isVowel(s[j]) {
+			j--
+			continue
+		}
+		b[i], b[j] = b[j], b[i]
+		i++
+		j--
+	}
+	return string(b)
+}
+
+func isVowel(c byte) bool {
+    if c < 'a' { 
+        c += 32 
+    }
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+}
