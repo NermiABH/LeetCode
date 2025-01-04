e3572ae6d3da4bbd2113f56738b25449e804c39f
NermiABH <93516873+NermiABH@users.noreply.github.com>
2025-01-04 14:06:51 +0300

    [1071] [Time Beats: 100.00%] [Memory Beats: 41.14%] - LeetPush

--- 1071-Greatest-Common-Divisor-of-Strings.go
+++ 1071-Greatest-Common-Divisor-of-Strings.go
@@ -6,29 +6,9 @@ func gcdNum(a, b int) int {
 }
 
 func gcdOfStrings(str1 string, str2 string) string {
-	if len(str1) < len(str2) {
-		str1, str2 = str2, str1
-	}
-Main:
-	for i := gcdNum(len(str2), len(str1)); i > 0; i /= 2 {
-		gcd := str2[:i]
-		for j := 0; ; j += i {
-			if gcd != str2[j:j+i] {
-				continue Main
-			}
-			if j+i >= len(str2) {
-				break
-			}
-		}
-		for j := 0; ; j += i {
-			if gcd != str1[j:i+j] {
-				continue Main
-			}
-			if j+i >= len(str1) {
-				break
-			}
-		}
-		return gcd
-	}
-	return ""
+	if str1 + str2 != str2 + str1 {
+        return ""
+    }
+    x := gcdNum(len(str1), len(str2))
+    return str1[:x]
 }
