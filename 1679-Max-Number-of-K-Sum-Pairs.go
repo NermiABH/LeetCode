732aefe551f380c89bacace0b0efd8be76bd5174
NermiABH <93516873+NermiABH@users.noreply.github.com>
2025-01-07 00:31:56 +0300

    [1679] [Time Beats: 99.28%] [Memory Beats: 88.43%] - LeetPush

--- 1679-Max-Number-of-K-Sum-Pairs.go
+++ 1679-Max-Number-of-K-Sum-Pairs.go
@@ -1,7 +1,13 @@
 func maxOperations(nums []int, k int) int {
+    if len(nums) < 2 {
+        return 0
+    }
 	n, out := make(map[int]int), 0
 	for _, v := range nums {
 		diff := k - v
+        if diff < 0 {
+            continue
+        }
 		if count := n[diff]; count == 0 {
 			n[v]++
 		} else {
