2ebfc6e21eb756d65d4bd9c3b22e55eefc253ec7
NermiABH <93516873+NermiABH@users.noreply.github.com>
2025-03-29 22:38:14 +0300

    [733] [Time Beats: 100.00%] [Memory Beats: 15.84%] - LeetPush

--- Flood-Fill.go
+++ Flood-Fill.go
@@ -1,36 +1,34 @@
-type Node struct {
-    i, j int
-    next *Node
-}
-
 func floodFill(image [][]int, sr int, sc int, color int) [][]int {
     oldColor := image[sr][sc]
     if oldColor == color {
         return image
     }
+
     m, n := len(image), len(image[0])
-    head := &Node{i: sr, j: sc, next: nil}
-    for head != nil {
-        i, j := head.i, head.j
-        head = head.next
-        if image[i][j] != oldColor {
-            continue
-        }
-        
-        image[i][j] = color
+    stack := [][2]int{{sr, sc}}
+    image[sr][sc] = color 
+
+    for len(stack) > 0 {
+        i, j := stack[len(stack)-1][0], stack[len(stack)-1][1]
+        stack = stack[:len(stack)-1]
 
         if i-1 >= 0 && image[i-1][j] == oldColor {
-            head = &Node{i: i - 1, j: j, next: head}
+            image[i-1][j] = color
+            stack = append(stack, [2]int{i - 1, j})
         }
         if i+1 < m && image[i+1][j] == oldColor {
-            head = &Node{i: i + 1, j: j, next: head}
+            image[i+1][j] = color
+            stack = append(stack, [2]int{i + 1, j})
         }
         if j-1 >= 0 && image[i][j-1] == oldColor {
-            head = &Node{i: i, j: j - 1, next: head}
+            image[i][j-1] = color
+            stack = append(stack, [2]int{i, j - 1})
         }
         if j+1 < n && image[i][j+1] == oldColor {
-            head = &Node{i: i, j: j + 1, next: head}
+            image[i][j+1] = color
+            stack = append(stack, [2]int{i, j + 1})
         }
     }
+
     return image
 }
