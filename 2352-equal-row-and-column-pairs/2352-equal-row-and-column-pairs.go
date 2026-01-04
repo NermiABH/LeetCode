0bf2a42e57ba72c9f0fa922ede6a774cff0e20a6
NermiABH <93516873+NermiABH@users.noreply.github.com>
2026-01-05 02:23:29 +0300

    Time: 1 ms (98.08%), Space: 9.4 MB (52.56%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.go
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.go
@@ -1,40 +1,21 @@
 func equalPairs(grid [][]int) int {
-	n := len(grid)
-	m := make(map[string]int, n)
+    n:=len(grid)
+    m:=make( map[[200]int]int)
+    arr:=[200]int{}
+    for i:=0;i<n;i++{
+        copy(arr[:],grid[i])
+        m[arr]++
+    }
+    res:=0
 
-	for i := 0; i < n; i++ {
-		b := make([]byte, n*4)
-		off := 0
-		for j := 0; j < n; j++ {
-			v := uint32(grid[i][j])
-			
-			b[off+0] = byte(v)
-			b[off+1] = byte(v >> 8)
-			b[off+2] = byte(v >> 16)
-			b[off+3] = byte(v >> 24)
-			off += 4
-		}
-		key := unsafe.String(unsafe.SliceData(b), len(b)) 
-		m[key]++
-	}
-
-
-	tmp := make([]byte, n*4)
-	res := 0
-
-	for c := 0; c < n; c++ {
-		off := 0
-		for r := 0; r < n; r++ {
-			v := uint32(grid[r][c])
-			tmp[off+0] = byte(v)
-			tmp[off+1] = byte(v >> 8)
-			tmp[off+2] = byte(v >> 16)
-			tmp[off+3] = byte(v >> 24)
-			off += 4
-		}
-		k := unsafe.String(unsafe.SliceData(tmp), len(tmp)) 
-		res += m[k]
-	}
-
-	return res
+    for i:=0;i<n;i++{
+        arr = [200]int{}
+        for j:=0;j<n;j++{ // copy column to arr
+            arr[j]=grid[j][i]
+        }
+        if v,ok:=m[arr];ok{
+            res+=v
+        }
+    }
+    return res
 }
