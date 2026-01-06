db2e5d6ae0f5008e877e07d73b8c513755aa2503
NermiABH <93516873+NermiABH@users.noreply.github.com>
2026-01-06 17:38:59 +0300

    Time: 15 ms (30.41%), Space: 8.6 MB (95.3%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.go
@@ -0,0 +1,15 @@
+import "strings"; 
+
+func removeStars(s string) string {
+    r := make([]rune, 0, len(s))
+
+    for _, v := range s {
+        if v == '*'{
+            r = r[:len(r)-1]   
+        }else {
+            r = append(r, v)
+        }
+    }
+
+    return string(r)
+}
